153c623269092b602c3b0f518ba2c5eba44367f7
Kylie Jo Swistak <kjswistak@gmail.com>
2020-04-28 15:51:52 -0500

    up rate to 50 messages

--- bot.rb
+++ bot.rb
@@ -1006,7 +1006,7 @@ bot.message do |event|
       #error_embed("Command not found!")
     #)
   elsif event.message.channel.id == 644771348073152522 && content.match(/clear chat/i)
-    msgs = event.message.channel.history(10)
+    msgs = event.message.channel.history(50)
     msgs.reject { |msg| msg.author.webhook? || msg.id == 651836628486062081 }
 
     event.message.channel.delete_messages(msgs)
