519a5a4f073ba0c172cbff56cbfc9962a5dfee03
Kylie Jo Swistak <kjswistak@gmail.com>
2020-05-02 21:46:22 -0500

    covering ass

--- bot.rb
+++ bot.rb
@@ -1199,7 +1199,7 @@ bot.reaction_add do |event|
               when /nsfw/i
                 ENV['CHAR_NSFW_CH']
               when /hidden/i
-                if user.current_bot?
+                if user&.current_bot?
                   event.channel.id
                 else
                   user.dm&.id
