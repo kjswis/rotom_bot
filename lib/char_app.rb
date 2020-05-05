f2e47e2afb1c60eb11ee96de8eb9a41982b64388
Kylie Jo Swistak <kjswistak@gmail.com>
2020-05-05 18:09:44 -0500

    update rejection responses

--- lib/char_app.rb
+++ lib/char_app.rb
@@ -2,24 +2,27 @@
 require_relative 'emoji.rb'
 
 module CharApp
-  GRAMMAR = "Please check your grammar and\ncapitalization"
-  UNITS = "Please specify your units in\nImperial or Metric"
-  IMAGE = "Your image is inappropriate for\ndefault use"
-  LORE = "One or more responses are\nconflicting with server lore"
-  UNDER_AGE = "Your age conflicts with the\nspecified rating"
-  INVALID = "One or more responses are\ninvalid"
-  VULGAR = "Your application is too vulgar,\nor conflicts with server rules"
-  DM_NOTES = "Please elaborate on your\nDM Notes"
+  GRAMMAR = "Please check your grammar and capitalization"
+  IMAGE = "Please check your units, grammar, and capitalization"
+  LORE = "Your image is inappropriate or conflicts with the age rating"
+  NONSENSE = "The character has conflicting personality traits or backstory"
+  DUPLICATE = "This character is either very similar to another, or does not have a proper reason to exist"
+  OVERPOWERED = "This character has unexplained abilities or is generally too powerful"
+  #GRAMMAR = "Please check your grammar and\ncapitalization"
+  #IMAGE = "Please check your units,\ngrammar, and capitalization"
+  #LORE = "Your image is inappropriate or\nconflicts with the age rating"
+  #NONSENSE = "The character has conflicting\npersonality traits or backstory"
+  #DUPLICATE = "This character is either very\nsimilar to another, or does\nnot have a proper reason to exist"
+  #OVERPOWERED = "This character has unexplained\nabilities or is generally\ntoo powerful"
   INLINE_SPACE = "------------------------------"
 
   REJECT_MESSAGES = {
     Emoji::SPEECH_BUBBLE => GRAMMAR,
-    Emoji::SCALE => UNITS,
     Emoji::PICTURE => IMAGE,
     Emoji::BOOKS => LORE,
-    Emoji::BABY => UNDER_AGE,
-    Emoji::SKULL => INVALID,
-    Emoji::VULGAR => VULGAR,
-    Emoji::NOTE => DM_NOTES
+    Emoji::NOTE => DM_NOTES,
+    Emoji::QUESTION => NONSENSE,
+    Emoji::PEOPLE => DUPLICATE,
+    Emoji::WIZARD => OVERPOWERED
   }
 end
