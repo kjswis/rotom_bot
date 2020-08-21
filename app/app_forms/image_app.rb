8e402dafe2a31614943646660db2172bef12299c
Kylie Jo Swistak <kjswistak@gmail.com>
2020-08-21 01:48:08 -0500

    spelling error

--- app/app_forms/image_app.rb
+++ app/app_forms/image_app.rb
@@ -28,7 +28,7 @@ class ImageApplication < ApplicationForm
     character = Character.find(image.char_id)
 
     # Determine appropriate channel
-    channel = image.category == 'NSFW' ? ENV['CHAR_NSFW_CH'] : ENV['CHAT_CH']
+    channel = image.category == 'NSFW' ? ENV['CHAR_NSFW_CH'] : ENV['CHAR_CH']
 
     reply = BotResponse.new(
       destination: channel,
