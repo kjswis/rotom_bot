aa039e9f6ae6758cd19eb54aff74b785f07c0f16
Kylie Jo Swistak <kjswistak@gmail.com>
2021-01-09 17:47:07 -0600

    detach compose up

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@ git pull
 
 docker-compose build
 docker-compose down
-docker-compose up
+docker-compose up -d
 #docker build . -t rotom_bot:latest
 #docker stop rotom_bot
 #docker run --env-file .env --name rotom_bot --rm -d rotom_bot:latest
